d2f6d3b5fe74f64953803c428f3220c97983706e
Marvin Borner <develop@marvinborner.de>
2019-10-29 00:13:33 +0100

    Added shifted keymap support

--- src/kernel/input/ps2/keyboard.c
+++ src/kernel/input/ps2/keyboard.c
@@ -2,11 +2,43 @@
 #include <kernel/io/io.h>
 #include <kernel/graphics/vesa.h>
 
+int shift_pressed = 0;
+
 char keymap[128] = {
         0 /*E*/, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
         '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
-        0 /*C*/, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0 /*LS*/,
-        '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0 /*RS*/, '*',
+        0 /*C*/, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 14 /*LS*/,
+        '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 14 /*RS*/, '*',
+        0, // Alt key
+        ' ', // Space bar
+        0, // Caps lock
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // F keys
+        0, // Num lock
+        0, // Scroll lock
+        0, // Home key
+        0, // Up arrow
+        0, // Page up
+        '-',
+        0, // Left arrow
+        0,
+        0, // Right arrow
+        '+',
+        0, // End key
+        0, // Down arrow
+        0, // Page down
+        0, // Insert key
+        0, // Delete key
+        0, 0, 0,
+        0, // F11
+        0, // F12
+        0, // Other keys
+};
+
+char shift_keymap[128] = {
+        0 /*E*/, 27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',
+        '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',
+        0 /*C*/, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~', 14 /*LS*/,
+        '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 14 /*RS*/, '*',
         0, // Alt key
         ' ', // Space bar
         0, // Caps lock
@@ -34,11 +66,20 @@ char keymap[128] = {
 
 void keyboard_handler(struct regs *r) {
     unsigned char scan_code;
+    char *current_keymap = keymap;
+    if (shift_pressed) current_keymap = shift_keymap;
 
     scan_code = receive_b(0x60);
 
-    if (!(scan_code & 0x80)) {
-        vesa_keyboard_char(keymap[scan_code]);
+    if (!(scan_code & 0x80)) { // PRESS
+        if (current_keymap[scan_code] == 14) {
+            shift_pressed = 1;
+            return;
+        }
+        vesa_keyboard_char(current_keymap[scan_code]);
+    } else if (scan_code & 0x80) { // RELEASE
+        char key = current_keymap[scan_code];
+        if (key == 14) shift_pressed = 0;
     }
 }
 
