b77b45f2db97215e596a83a54991bd728feb278b
Marvin Borner <develop@marvinborner.de>
2019-10-29 22:45:59 +0100

    Fixed multi-byte font conversion

--- src/kernel/graphics/conv.sh
+++ src/kernel/graphics/conv.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # bdf2c output formatter
-# TODO: Currently only works with 1 byte widths
 
 font_url="https://raw.githubusercontent.com/fcambus/spleen/master/spleen-8x16.bdf"
 character_count=758
@@ -19,6 +18,7 @@ sed -i -e 1,74d final.h
 sed -i -e :a -e '$d;N;2,1533ba' -e 'P;D' final.h
 
 sed -i "1s/.*/$top_lines/" final.h
+sed -i 's/\,0x//g' final.h
 sed -i -z "s/$font_height\n /$font_height\n        \{/g" final.h
 sed -i -z 's/\,\n\/\//\}\,\n\/\//g' final.h
 sed -i 's/\/\/ [0-9]/        \/\/ /g' final.h
