e4b897bd9353990918225a34937cd8723b4e1538
Marvin Borner <develop@marvinborner.de>
2020-05-28 23:48:01 +0200

    Fixed clean buffer and added TODO :)

--- src/kernel/lib/stdio/vprintf.c
+++ src/kernel/lib/stdio/vprintf.c
@@ -1,11 +1,14 @@
 #include <lib/stdio.h>
 #include <lib/stdlib.h>
+#include <lib/lib.h>
 #include <stdint.h>
 
 // TODO: Use global formatting function
+// TODO: Fix fixed buffer size
 void vprintf(const char *fmt, va_list args)
 {
 	char buf[1024];
+	memset(buf, 0, 1024);
 	vsprintf(buf, fmt, args);
 
 	for (u32 i = 0; i < strlen(buf); i++)
