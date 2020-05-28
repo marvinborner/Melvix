f784575fdbbee6e096866c0b506fd660dc5ffe1c
Marvin Borner <develop@marvinborner.de>
2020-05-28 19:40:44 +0200

    Started higher-half kernel

--- src/kernel/boot.asm
+++ src/kernel/boot.asm
@@ -1,4 +1,23 @@
-section .multiboot
+bits 32
+
+; Some variables
+kernel_virt_base equ 0xC0000000
+kernel_page_number equ (kernel_virt_base >> 22)
+kernel_stack equ 0x4000
+
+section .data
+	align 0x1000
+
+	boot_page_dir:
+		dd 0x00000083
+		times (kernel_page_number - 1) dd 0
+		dd 0x00000083
+		times (1024 - kernel_page_number - 1) dd 0
+
+section .text
+	align 4
+
+	multiboot:
 	header_start:
 		dd 0xe85250d6
 		dd 0
@@ -15,6 +34,12 @@ section .multiboot
 		dd 6 ; mmap
 		dd 13 ; smbios
 
+		; Page-align tag
+		align 8
+		dw 6
+		dw 0
+		dd 8
+
 		; Empty tag
 		align 8
 		dw 0
@@ -22,29 +47,36 @@ section .multiboot
 		dd 8
 	header_end:
 
-section .start_section
-	dd _start
-
-; Initialize stack
-;section .bss
-;	align 16
-;
-;	STACK_BOTTOM:
-;		resb 0x4000
-;	STACK_TOP:
-
-section .text
-	global _start
+	global boot
 	extern kernel_main
-	_start:
-		;mov esp, STACK_TOP
+	boot:
+		mov ecx, (boot_page_dir - kernel_virt_base)
+		mov cr3, ecx
+
+		mov ecx, cr4
+		or ecx, 0x00000010
+		mov cr4, ecx
+		
+		mov ecx, cr0
+		or ecx, 0x80000000
+		mov cr0, ecx
+
+		lea ecx, [higher_half]
+		jmp ecx
+
+	higher_half:
+		mov dword [boot_page_dir], 0
+		invlpg [0]
+		
+		mov esp, stack + kernel_stack
 		push esp
 		push ebx
 		push eax
 		cli
 		call kernel_main
-		jmp $
+		hlt
 
-section .end
-	global KERNEL_END
-	KERNEL_END:
+section .bss
+	align 32
+	stack:
+		resb kernel_stack
