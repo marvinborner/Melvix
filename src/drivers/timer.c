9a214359cc8c8f944825fa27aed188e0af5f8329
Marvin Borner <develop@marvinborner.de>
2020-07-26 18:26:55 +0200

    Added cpu timer
    Well, it doesn't work *at all*. When I include these files with the
    Makefile everything crashes. I *think* this is due to the fact that the
    bootloader only handles direct ext2 pointer and the kernel is too big to
    fit into them. Therefore the kernel taps into the void as it tries to
    read some data an crashes.
    
    It could be something completely different though - let's see!
    (this will take some time ig)

---
+++ src/drivers/timer.c
@@ -0,0 +1,39 @@
+// MIT License, Copyright (c) 2020 Marvin Borner
+
+#include <cpu.h>
+#include <def.h>
+#include <interrupts.h>
+
+static u32 timer_ticks = 0;
+
+void timer_phase(int hz)
+{
+	int divisor = 3579545 / 3 / hz;
+	outb(0x43, 0x36); // 01 10 11 0b // CTR, RW, MODE, BCD
+	outb(0x40, divisor & 0xFF);
+	outb(0x40, divisor >> 8);
+}
+
+// Executed 1000 times per second
+void timer_handler()
+{
+	timer_ticks++;
+}
+
+// "Delay" function with CPU sleep
+void timer_wait(u32 ticks)
+{
+	u32 eticks;
+
+	eticks = timer_ticks + ticks;
+	while (timer_ticks < eticks) {
+		__asm__("sti//hlt//cli");
+	}
+}
+
+// Install timer handler into IRQ0
+void timer_install()
+{
+	/* timer_phase(1000); */
+	irq_install_handler(0, timer_handler);
+}
---
+++ src/inc/timer.h
@@ -0,0 +1,10 @@
+// MIT License, Copyright (c) 2020 Marvin Borner
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <def.h>
+
+void timer_install();
+
+#endif
