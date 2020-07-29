be0246208d1fe7727f94221ea7562f9a9cce1659
Marvin Borner <develop@marvinborner.de>
2020-07-29 15:02:58 +0200

    Forget what I said, this was an easy fix
    Making the asm instructions volatile seems to fix every issue

--- src/drivers/cpu.c
+++ src/drivers/cpu.c
@@ -6,44 +6,43 @@
 u8 inb(u16 port)
 {
 	u8 value;
-	__asm__("inb %1, %0" : "=a"(value) : "Nd"(port));
+	__asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port));
 	return value;
 }
 
 u16 inw(u16 port)
 {
 	u16 value;
-	__asm__("inw %1, %0" : "=a"(value) : "Nd"(port));
+	__asm__ volatile("inw %1, %0" : "=a"(value) : "Nd"(port));
 	return value;
 }
 
 u32 inl(u16 port)
 {
 	u32 value;
-	__asm__("inl %1, %0" : "=a"(value) : "Nd"(port));
+	__asm__ volatile("inl %1, %0" : "=a"(value) : "Nd"(port));
 	return value;
 }
 
-// TODO: Fix optimization issues with insl
-void __attribute__((optimize("O0"))) insl(u16 port, void *addr, int n)
+void insl(u16 port, void *addr, int n)
 {
-	__asm__("cld; rep insl"
-		: "=D"(addr), "=c"(n)
-		: "d"(port), "0"(addr), "1"(n)
-		: "memory", "cc");
+	__asm__ volatile("cld; rep insl"
+			 : "=D"(addr), "=c"(n)
+			 : "d"(port), "0"(addr), "1"(n)
+			 : "memory", "cc");
 }
 
 void outb(u16 port, u8 data)
 {
-	__asm__("outb %0, %1" ::"a"(data), "Nd"(port));
+	__asm__ volatile("outb %0, %1" ::"a"(data), "Nd"(port));
 }
 
 void outw(u16 port, u16 data)
 {
-	__asm__("outw %0, %1" ::"a"(data), "Nd"(port));
+	__asm__ volatile("outw %0, %1" ::"a"(data), "Nd"(port));
 }
 
 void outl(u16 port, u32 data)
 {
-	__asm__("outl %0, %1" ::"a"(data), "Nd"(port));
+	__asm__ volatile("outl %0, %1" ::"a"(data), "Nd"(port));
 }
