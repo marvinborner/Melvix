dc661fee8168ed46b06b682f80f1dcab9e69e7be
Marvin Borner <develop@marvinborner.de>
2020-11-26 14:04:50 +0100

    Added userspace heap

--- libc/inc/mem.h
+++ libc/inc/mem.h
@@ -13,9 +13,8 @@ void heap_init(u32 start);
 void *malloc(u32 size);
 void free(void *ptr);
 #elif defined(userspace)
-#include <sys.h>
-#define malloc(n) (void *)sys1(SYS_MALLOC, n)
-#define free(ptr) (void)(sys1(SYS_FREE, (int)ptr))
+void *malloc(u32 size);
+void free(void *ptr);
 #else
 #error "No lib target specified. Please use -Dkernel or -Duserspace"
 #endif
