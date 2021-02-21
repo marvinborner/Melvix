86eab6b83823c252066b57a7c2f866089325a146
Marvin Borner <develop@marvinborner.de>
2021-02-21 23:15:54 +0100

    Started rectangle generation

--- apps/wm.c
+++ apps/wm.c
@@ -9,6 +9,8 @@
 #include <random.h>
 #include <vesa.h>
 
+//#define FLUSH_TIMEOUT 6
+
 struct client {
 	u32 pid;
 };
@@ -19,17 +21,18 @@ struct window {
 	struct context ctx;
 	struct client client;
 	u32 flags;
-	vec3 pos;
-	vec3 pos_prev;
+	vec2 pos;
+	vec2 pos_prev;
 };
 
 struct rectangle {
 	vec2 pos1; // Upper left
 	vec2 pos2; // Lower right
+	void *data;
 };
 
 static struct vbe screen = { 0 };
-static struct list *windows = NULL;
+static struct list *windows = NULL; // THIS LIST SHALL BE SORTED BY Z-INDEX!
 static struct window *root = NULL;
 static struct window *direct = NULL;
 static struct window *cursor = NULL;
@@ -47,7 +50,7 @@ static struct {
 	u8 right : 1;
 } mouse = { 0 };
 
-static struct window *window_create(struct client client, const char *name, struct vec3 pos,
+static struct window *window_create(struct client client, const char *name, struct vec2 pos,
 				    struct vec2 size, u32 flags)
 {
 	struct window *win = malloc(sizeof(*win));
@@ -86,20 +89,78 @@ static void window_destroy(struct window *win)
 
 static void buffer_flush()
 {
+#ifdef FLUSH_TIMEOUT
+	static u32 time_flush = 0;
+	u32 time_now = time();
+	if (time_now - time_flush > FLUSH_TIMEOUT) {
+		memcpy(direct->ctx.fb, root->ctx.fb, root->ctx.bytes);
+		time_flush = time_now;
+	}
+#else
 	memcpy(direct->ctx.fb, root->ctx.fb, root->ctx.bytes);
+#endif
+}
+
+static void windows_at_rec(vec2 pos1, vec2 pos2, struct list *list)
+{
+	struct node *iterator = windows->head;
+	while (iterator) {
+		struct window *win = iterator->data;
+		u8 starts_in = (win->pos.x > pos1.x && win->pos.x < pos2.x) &&
+			       (win->pos.y > pos1.y && win->pos.y < pos2.y);
+		if (starts_in)
+			list_add(list, win);
+		iterator = iterator->next;
+	}
+}
+
+static struct rectangle rectangle_at(vec2 pos1, vec2 pos2, struct window *excluded)
+{
+	u32 width = pos2.x - pos1.x;
+	u32 height = pos2.y - pos1.y;
+	void *data = malloc(width * height * 4);
+
+	struct list *windows_at = list_new();
+	windows_at_rec(pos1, pos2, windows_at);
+	struct node *iterator = windows_at->head;
+	while (iterator) {
+		struct window *win = iterator->data;
+
+		/* int bypp = win->ctx.bpp >> 3; */
+		/* u8 *srcfb = &win->ctx.fb[pos1.x * bypp + pos1.y * win->ctx.pitch]; */
+		/* u8 *destfb = data; */
+		/* u32 cnt = 0; */
+		/* for (u32 cy = 0; cy < height; cy++) { */
+		/* 	memcpy(destfb, srcfb, width * bypp); */
+		/* 	srcfb += win->ctx.pitch; */
+		/* 	destfb += win->ctx.pitch; */
+		/* 	cnt += win->ctx.pitch; */
+		/* } */
+
+		iterator = iterator->next;
+
+		if (win == excluded)
+			continue;
+
+		log("Window found: %s\n", win->name);
+	}
+	list_destroy(windows_at);
+
+	return (struct rectangle){ .pos1 = pos1, .pos2 = pos2, .data = data };
 }
 
 static void redraw_window(struct window *win)
 {
 	if (win->ctx.size.x == win->ctx.size.y) {
-		// TODO: Evaluate rectangle at pos for redraw
-		gfx_draw_rectangle(&root->ctx, vec3to2(win->pos_prev),
+		// TODO: Redraw rectangle
+		rectangle_at(win->pos_prev, vec2_add(win->pos_prev, win->ctx.size), win);
+		gfx_draw_rectangle(&root->ctx, win->pos_prev,
 				   vec2_add(win->pos_prev, win->ctx.size), 0);
 	} else {
 		err(1, "Rectangle splitting isn't supported yet!\n");
 	}
 
-	gfx_ctx_on_ctx(&root->ctx, &win->ctx, vec3to2(win->pos));
+	gfx_ctx_on_ctx(&root->ctx, &win->ctx, win->pos);
 	buffer_flush();
 }
 
@@ -135,7 +196,7 @@ static void handle_event_mouse(struct event_mouse *event)
 		return;
 	}
 
-	cursor->pos_prev = vec2to3(mouse.pos, U32_MAX);
+	cursor->pos_prev = mouse.pos;
 
 	mouse.pos.x += event->diff_x;
 	mouse.pos.y -= event->diff_y;
@@ -152,8 +213,8 @@ static void handle_event_mouse(struct event_mouse *event)
 	else if (mouse.pos.y + cursor->ctx.size.y > (unsigned)screen.height - 1)
 		mouse.pos.y = screen.height - cursor->ctx.size.y - 1;
 
-	//log("%d %d\n", mouse.pos.x, mouse.pos.y);
-	cursor->pos = vec2to3(mouse.pos, U32_MAX);
+	/* log("%d %d\n", mouse.pos.x, mouse.pos.y); */
+	cursor->pos = mouse.pos;
 
 	redraw_window(cursor);
 }
@@ -168,18 +229,17 @@ int main(int argc, char **argv)
 	windows = list_new();
 	keymap = keymap_parse("/res/keymaps/en.keymap");
 
-	direct =
-		window_create(wm_client, "direct", vec3(0, 0, 0), vec2(screen.width, screen.height),
-			      WF_NO_FB | WF_NO_DRAG | WF_NO_FOCUS | WF_NO_RESIZE);
+	direct = window_create(wm_client, "direct", vec2(0, 0), vec2(screen.width, screen.height),
+			       WF_NO_FB | WF_NO_DRAG | WF_NO_FOCUS | WF_NO_RESIZE);
 	direct->ctx.fb = screen.fb;
 	direct->flags ^= WF_NO_FB;
-	root = window_create(wm_client, "root", vec3(0, 0, 0), vec2(screen.width, screen.height),
+	root = window_create(wm_client, "root", vec2(0, 0), vec2(screen.width, screen.height),
 			     WF_NO_DRAG | WF_NO_FOCUS | WF_NO_RESIZE);
-	cursor = window_create(wm_client, "cursor", vec3(0, 0, 0), vec2(32, 32),
+	cursor = window_create(wm_client, "cursor", vec2(0, 0), vec2(32, 32),
 			       WF_NO_DRAG | WF_NO_FOCUS | WF_NO_RESIZE);
 
 	/* gfx_write(&direct->ctx, vec2(0, 0), FONT_32, COLOR_FG, "Loading Melvix..."); */
-	/* gfx_load_wallpaper(&direct->ctx, "/res/wall.png"); */
+	gfx_load_wallpaper(&root->ctx, "/res/wall.png");
 	gfx_load_wallpaper(&cursor->ctx, "/res/cursor.png");
 
 	struct message msg = { 0 };
