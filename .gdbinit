1d7f02567a703c266dfc4e810cc79afdf7926601
Marvin Borner <develop@marvinborner.de>
2021-09-28 15:40:18 +0200

    Clean slate

--- .gdbinit
+++
@@ -1,2 +0,0 @@
-file build/kernel.elf
-target remote localhost:1234
--- kernel/inc/management/dev/detection.h
+++
@@ -1,8 +0,0 @@
-// MIT License, Copyright (c) 2021 Marvin Borner
-
-#ifndef MANAGEMENT_DEV_DETECTION_H
-#define MANAGEMENT_DEV_DETECTION_H
-
-void dev_detect(void);
-
-#endif
